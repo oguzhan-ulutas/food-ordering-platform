9e4e6be9a3aa9b93531d6efe3458250a994b0586
Oguzhan Ulutas <mkoulutas@gmail.com>
2024-05-01 23:05:11 +0300

    Init server

---
+++ server/src/index.ts
@@ -0,0 +1,17 @@
+import express, {Request, Response} from "express"
+import cors from "cors"
+import "dotenv/config"
+
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.get("/test", async (req:Request, res:Response)=>{
+    res.json({message:"Hello"})
+})
+
+app.listen(7001, ()=>{
+    console.log("Server started at localhost:7001!")
+})
