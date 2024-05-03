22bbd47b65544f69c91a450aff29eda3e40c600b
Oguzhan Ulutas <mkoulutas@gmail.com>
2024-05-03 19:18:48 +0300

    Add HomePage

---
+++ client/src/components/Hero.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import hero from "../assets/hero.png"
+
+const Hero = () => {
+  return (
+    <div>
+        <img src={hero} alt="Burger picture." className='w-full max-h-[600px] object-cover' />
+    </div>
+  )
+}
+
+export default Hero
