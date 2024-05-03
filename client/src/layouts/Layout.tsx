1428bbdb83c53ff774f2c04879997434c0cd04cd
Oguzhan Ulutas <mkoulutas@gmail.com>
2024-05-03 20:18:50 +0300

    Add Auth0

--- client/src/layouts/Layout.tsx
+++ client/src/layouts/Layout.tsx
@@ -1,3 +1,4 @@
+import Footer from '@/components/Footer'
 import Header from '@/components/Header'
 import Hero from '@/components/Hero'
 import React from 'react'
@@ -12,6 +13,7 @@ export const Layout = ({children}:Props) => {
         <Header/>
         <Hero />
         <div className='container mx-auto flex-1 py-10 ' >{children}</div>
+        <Footer />
     </div>
   )
 }
