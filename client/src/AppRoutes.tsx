34fb32b9f8728cf490777cea9985b3477f6756a8
Oguzhan Ulutas <mkoulutas@gmail.com>
2024-05-03 17:33:12 +0300

    Add header

--- client/src/AppRoutes.tsx
+++ client/src/AppRoutes.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
+import { Layout } from './layouts/Layout'
 
 const AppRoutes = () => {
   return (
     <Routes>
-        <Route path='/' element={<span>Home Page</span>} />
+        <Route path='/' element={<Layout>Home Page</Layout>} />
         <Route path='/user-profile' element={<span>User Profile Page</span>} />
         <Route path='*' element={<Navigate to="/" />} />
     </Routes>
---
+++ client/src/layouts/Layout.tsx
@@ -0,0 +1,15 @@
+import Header from '@/components/Header'
+import React from 'react'
+
+type Props = {
+    children: React.ReactNode
+}
+
+export const Layout = ({children}:Props) => {
+  return (
+    <div className='flex flex-col min-h-screen' >
+        <Header/>
+        <div className='container mx-auto flex-1 py-10 ' >{children}</div>
+    </div>
+  )
+}
