d863dded19a33a091e758125709b3a086fdc4d9f
Oguzhan Ulutas <mkoulutas@gmail.com>
2024-05-05 22:36:01 +0300

    Add user drop down menu

--- client/src/auth/Auth0ProviderWithNavigate.tsx
+++ client/src/auth/Auth0ProviderWithNavigate.tsx
@@ -8,15 +8,15 @@ const Auth0ProviderWithNavigate = ({children}:Props) => {
     const domain = import.meta.env.VITE_AUTH0_DOMAIN
     const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
     const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
-    const audience = import.meta.env.VITE_AUTH0_AUDIENCE;
+    // const audience = import.meta.env.VITE_AUTH0_AUDIENCE;
 
-    if (!domain || !clientId || redirectUri || audience) {
+    if (!domain || !clientId || !redirectUri ) {
         throw new Error("unable to initialise auth")
     }
 
     const onRedirectCallback = (appState?: AppState, user?: User) => {
         console.log("User :", user)
-        navigate(appState?.returnTo || "/auth-callback");
+        // navigate(appState?.returnTo || "/auth-callback");
       };
 
   return (
@@ -25,7 +25,7 @@ const Auth0ProviderWithNavigate = ({children}:Props) => {
         clientId={clientId}
         authorizationParams={{
             redirect_uri: redirectUri,
-            audience,
+
       }}
       onRedirectCallback={onRedirectCallback} >
         {children}
