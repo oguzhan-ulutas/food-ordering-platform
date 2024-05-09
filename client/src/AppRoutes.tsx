9e9aca01592c3b59f22c90d5220a01a1356db10a
Oguzhan Ulutas <mkoulutas@gmail.com>
2024-05-09 23:03:58 +0300

    Clear auth provider

--- client/src/AppRoutes.tsx
+++ client/src/AppRoutes.tsx
@@ -1,16 +1,25 @@
-import React from 'react'
-import { Navigate, Route, Routes } from 'react-router-dom'
-import { Layout } from './layouts/Layout'
-import HomePage from './pages/HomePage'
+import React from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { Layout } from "./layouts/Layout";
+import HomePage from "./pages/HomePage";
+import AuthCallbackPage from "./pages/AuthCallbackPage";
 
 const AppRoutes = () => {
   return (
     <Routes>
-        <Route path='/' element={<Layout><HomePage/></Layout>} />
-        <Route path='/user-profile' element={<span>User Profile Page</span>} />
-        <Route path='*' element={<Navigate to="/" />} />
+      <Route
+        path="/"
+        element={
+          <Layout>
+            <HomePage />
+          </Layout>
+        }
+      />
+      <Route path="/auth-callback" element={<AuthCallbackPage />} />
+      <Route path="/user-profile" element={<span>User Profile Page</span>} />
+      <Route path="*" element={<Navigate to="/" />} />
     </Routes>
-  )
-}
+  );
+};
 
-export default AppRoutes
+export default AppRoutes;
--- client/src/auth/Auth0ProviderWithNavigate.tsx
+++ client/src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,12 +1,11 @@
-import { useCreateMyUser } from "@/api/MyUserApi";
-import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
+import { Auth0Provider } from "@auth0/auth0-react";
 import { useNavigate } from "react-router-dom";
 
 type Props = {
   children: React.ReactNode;
 };
 const Auth0ProviderWithNavigate = ({ children }: Props) => {
-  const { createUser } = useCreateMyUser();
+  const navigate = useNavigate();
 
   const domain = import.meta.env.VITE_AUTH0_DOMAIN;
   const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
@@ -17,11 +16,8 @@ const Auth0ProviderWithNavigate = ({ children }: Props) => {
     throw new Error("unable to initialize auth");
   }
 
-  const onRedirectCallback = (appState?: AppState, user?: User) => {
-    if (user?.sub && user?.email) {
-      createUser({ auth0Id: user.sub, email: user.email });
-    }
-    // navigate(appState?.returnTo || "/auth-callback");
+  const onRedirectCallback = () => {
+    navigate("/auth-callback");
   };
 
   return (
