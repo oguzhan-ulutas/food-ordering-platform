e4f194547c2cbbabf5b763988c30f673b1e13360
Oguzhan Ulutas <mkoulutas@gmail.com>
2024-05-20 18:16:09 +0300

    Add Search page logic

--- client/src/pages/SearchPage.tsx
+++ client/src/pages/SearchPage.tsx
@@ -1,9 +1,32 @@
+import { useSearchRestaurants } from "@/api/RestaurantApi";
 import { useParams } from "react-router-dom";
+
+// export type SearchState = {
+//   searchQuery: string;
+//   page: number;
+//   selectedCuisines: string[];
+//   sortOption: string;
+// };
 
 const SearchPage = () => {
   const { city } = useParams();
 
-  return <span>User search for {city}</span>;
+  const { results } = useSearchRestaurants(city);
+
+  return (
+    <span>
+      User search for {city}
+      <span>
+        found -
+        {results?.data.map((restaurant) => (
+          <span>
+            {" "}
+            {restaurant.restaurantName} {restaurant.city}{" "}
+          </span>
+        ))}
+      </span>{" "}
+    </span>
+  );
 };
 
 export default SearchPage;
